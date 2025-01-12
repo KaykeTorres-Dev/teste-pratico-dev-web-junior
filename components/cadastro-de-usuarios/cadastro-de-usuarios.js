6da44443779964372a36082a7749e8bf659ea246
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-01-12 19:07:14 -0300

    Mlehorias no código e adição de classes nos inputs para feedback visual das validações.

--- components/cadastro-de-usuarios/cadastro-de-usuarios.js
+++ components/cadastro-de-usuarios/cadastro-de-usuarios.js
@@ -1,7 +1,14 @@
 function checkNameInput() {
-    const userName = document.getElementById("userName").value;
-    nameValidationFeedback.textContent = userName !== "" ? "" : "Preencha o campo nome";
-    nameValidationFeedback.style.color = userName !== "" ? "" : "red";
+    const userName = document.getElementById("userName");
+    nameValidationFeedback.textContent = userName.value !== "" ? "" : "Preencha o campo nome";
+    nameValidationFeedback.style.color = userName.value !== "" ? "" : "red";
+
+    if (userName.value !== "") {
+        userName.classList.remove("is-invalid"); 
+        userName.classList.add("is-valid"); 
+    } else {
+        userName.classList.add("is-invalid"); 
+    }
 }
 
 const validateEmail = (email) => {
@@ -10,48 +17,56 @@ const validateEmail = (email) => {
 };
 
 function checkEmailInput() { 
-    const userEmail = document.getElementById("userEmail").value;
-    const isEmailValid = validateEmail(userEmail);
+    const userEmail = document.getElementById("userEmail");
+    const isEmailValid = validateEmail(userEmail.value);
 
-    if (userEmail !== "") {
+    if (userEmail.value !== "") {
         if (!isEmailValid) {
             emailValidationFeedback.textContent = "Email inválido";
             emailValidationFeedback.style.color = "red";
+            userEmail.classList.add("is-invalid"); 
             return;
                         
         } else {
             emailValidationFeedback.textContent = "";
             emailValidationFeedback.style.color = "";
+            userEmail.classList.remove("is-invalid"); 
+            userEmail.classList.add("is-valid"); 
         }   
 
     } else {
         emailValidationFeedback.textContent = "Preencha o campo email";
         emailValidationFeedback.style.color = "red"; 
+        userEmail.classList.add("is-invalid"); 
     }
 }
 
 function checkPasswordInput() {
-    const userPassword = document.getElementById("userPassword").value;
+    const userPassword = document.getElementById("userPassword");
 
-    if (userPassword !== "") {
-        if (userPassword.length > 0 && userPassword.length < 5) {
+    if (userPassword.value !== "") {
+        if (userPassword.value.length > 0 && userPassword.value.length < 5) {
             passwordValidationFeedback.textContent = "A senha tem que ter no mínimo 6 caracteres";
             passwordValidationFeedback.style.color = "red";
+            userPassword.classList.add("is-invalid"); 
         } else {
             passwordValidationFeedback.textContent = "";
             passwordValidationFeedback.style.color = "";
+            userPassword.classList.remove("is-invalid"); 
+            userPassword.classList.add("is-valid");
         }
 
     } else {
         passwordValidationFeedback.textContent = "Preencha o campo senha";
-        passwordValidationFeedback.style.color = "red";
+        passwordValidationFeedback.style.color = "red"; 
+        userPassword.classList.add("is-invalid"); 
     }
 }
 
 function createUser() {
-    const userName = document.getElementById("userName").value;
-    const userEmail = document.getElementById("userEmail").value;
-    const userPassword = document.getElementById("userPassword").value;
+    const userName = document.getElementById("userName");
+    const userEmail = document.getElementById("userEmail");
+    const userPassword = document.getElementById("userPassword");
     const nameValidationFeedback = document.getElementById("nameValidationFeedback");
     const emailValidationFeedback = document.getElementById("emailValidationFeedback");
     const passwordValidationFeedback = document.getElementById("passwordValidationFeedback");
@@ -67,19 +82,24 @@ function createUser() {
         "all": "Preencha todos os campos",
         "name": "Preencha o campo nome",
         "email": "Preencha o campo email",
-        "password": "Preencha o campo senha",
+        "password": "Preencha o campo senha"
     };
   
     const missingFields = [];
   
-    if (userName === "") {
+    if (userName.value === "") {
       missingFields.push("name");
+      userName.classList.add("is-invalid"); 
     }
-    if (userEmail === "") {
+
+    if (userEmail.value === "") {
       missingFields.push("email");
+      userEmail.classList.add("is-invalid"); 
     }
-    if (userPassword === "") {
+
+    if (userPassword.value === "") {
       missingFields.push("password");
+      userPassword.classList.add("is-invalid"); 
     }
   
     
@@ -93,9 +113,9 @@ function createUser() {
     }
 
     const userInfoObject = {
-        name: userName,
-        email: userEmail,
-        password: userPassword
+        name: userName.value,
+        email: userEmail.value,
+        password: userPassword.value
     }
                
     // TODO: Fazer o crud para realizar o envio dos dados do usuário para o server.
