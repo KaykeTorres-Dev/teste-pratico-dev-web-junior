005d68d185eebb6bcc3eee3be1904054dc739209
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-01-12 18:32:32 -0300

    Implementação das validações dos campos nome, e-mail e senha, limpeza de código em cadastro-de-usuarios.js

--- components/cadastro-de-usuarios/cadastro-de-usuarios.js
+++ components/cadastro-de-usuarios/cadastro-de-usuarios.js
@@ -1,50 +1,105 @@
+function checkNameInput() {
+    const userName = document.getElementById("userName").value;
+    nameValidationFeedback.textContent = userName !== "" ? "" : "Preencha o campo nome";
+    nameValidationFeedback.style.color = userName !== "" ? "" : "red";
+}
+
+const validateEmail = (email) => {
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
+    return regex.test(email);
+};
+
+function checkEmailInput() { 
+    const userEmail = document.getElementById("userEmail").value;
+    const isEmailValid = validateEmail(userEmail);
+
+    if (userEmail !== "") {
+        if (!isEmailValid) {
+            emailValidationFeedback.textContent = "Email inválido";
+            emailValidationFeedback.style.color = "red";
+            return;
+                        
+        } else {
+            emailValidationFeedback.textContent = "";
+            emailValidationFeedback.style.color = "";
+        }   
+
+    } else {
+        emailValidationFeedback.textContent = "Preencha o campo email";
+        emailValidationFeedback.style.color = "red"; 
+    }
+}
+
+function checkPasswordInput() {
+    const userPassword = document.getElementById("userPassword").value;
+
+    if (userPassword !== "") {
+        if (userPassword.length > 0 && userPassword.length < 5) {
+            passwordValidationFeedback.textContent = "A senha tem que ter no mínimo 6 caracteres";
+            passwordValidationFeedback.style.color = "red";
+        } else {
+            passwordValidationFeedback.textContent = "";
+            passwordValidationFeedback.style.color = "";
+        }
+
+    } else {
+        passwordValidationFeedback.textContent = "Preencha o campo senha";
+        passwordValidationFeedback.style.color = "red";
+    }
+}
+
 function createUser() {
-    let userName = document.getElementById("userName").value;
-    let userEmail = document.getElementById("userEmail").value;
-    let userPassword = document.getElementById("userPassword").value;
-    let isFormUserNull = false;
-    let isUserNameInputNull = false;
-    let isUserEmailInputNull = false;
-    let isUserPasswordInputNull = false;
+    const userName = document.getElementById("userName").value;
+    const userEmail = document.getElementById("userEmail").value;
+    const userPassword = document.getElementById("userPassword").value;
+    const nameValidationFeedback = document.getElementById("nameValidationFeedback");
+    const emailValidationFeedback = document.getElementById("emailValidationFeedback");
+    const passwordValidationFeedback = document.getElementById("passwordValidationFeedback");
+  
+    nameValidationFeedback.textContent = "";
+    nameValidationFeedback.style.color = "";
+    emailValidationFeedback.textContent = "";
+    emailValidationFeedback.style.color = "";
+    passwordValidationFeedback.textContent = "";
+    passwordValidationFeedback.style.color = "";
 
-   
-    if (userName == "" && userEmail == "" && userPassword == "") {
-        console.log("Preencha todos os campos !!!");  
-        isFormUserNull = true;
-       
-         
-    } else if (userName !== "" && userEmail == "" && userPassword == "") {
-        console.log("Preencha os campos de email e senha!!!"); 
-        isUserEmailInputNull = true;  
-        isUserPasswordInputNull = true;  
+    const validationMessages = {
+        "all": "Preencha todos os campos",
+        "name": "Preencha o campo nome",
+        "email": "Preencha o campo email",
+        "password": "Preencha o campo senha",
+    };
+  
+    const missingFields = [];
+  
+    if (userName === "") {
+      missingFields.push("name");
+    }
+    if (userEmail === "") {
+      missingFields.push("email");
+    }
+    if (userPassword === "") {
+      missingFields.push("password");
+    }
+  
+    
+    if (missingFields.length > 0) {
+        for (const field of missingFields) {
+        const feedbackElement = document.getElementById(`${field}ValidationFeedback`);
+        feedbackElement.textContent = validationMessages[field];
+        feedbackElement.style.color = "red";
+        }
+        return;
+    }
 
-    } else if (userName == "" && userEmail !== "" && userPassword == "") {
-        console.log("Preencha os campos de nome e senha!!!"); 
-        isUserNameInputNull = true;  
-        isUserPasswordInputNull = true;  
-
-    } else if (userName == "" && userEmail == "" && userPassword !== "") {
-        console.log("Preencha os campos de nome e email!!!"); 
-        isUserNameInputNull = true;  
-        isUserEmailInputNull = true;  
-       
-    } else if (userName == "" && userEmail !== "" && userPassword !== "") {
-        console.log("Preencha o campo nome!!!"); 
-        isUserNameInputNull = true;
-
-
-    } else if (userName !== "" && userEmail == "" && userPassword !== "") {
-        console.log("Preencha o campo email!!!"); 
-        isUserEmailInputNull = true;  
-
-       
-    } else if (userName !== "" && userEmail != "" && userPassword == "") {
-        console.log("Preencha o campo senha!!!"); 
-        isUserPasswordInputNull = true; 
-
-    }  else {
-        console.log("Pronto para cadastrar");
-        window.location.href = "http://127.0.0.1:5500/listagem-de-usuarios/listagem-de-usuarios.html";
-    }  
-
+    const userInfoObject = {
+        name: userName,
+        email: userEmail,
+        password: userPassword
+    }
+               
+    // TODO: Fazer o crud para realizar o envio dos dados do usuário para o server.
+    
+    console.log("Pronto para cadastrar", userInfoObject);
+    // window.location.href = "http://127.0.0.1:5500/listagem-de-usuarios/listagem-de-usuarios.html";
 }
