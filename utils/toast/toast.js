c88ffff07412334291a5436b97c99108df13bf14
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-01-16 16:27:52 -0300

    Padronização de pontuação para se adequar ao projeto

--- utils/toast/toast.js
+++ utils/toast/toast.js
@@ -1,23 +1,23 @@
-function toast(message, type = 'info') {
+function toast(message, type = "info") {
 
-    const toastContainer = document.createElement('div');
-    toastContainer.classList.add('toast-container', 'position-fixed','p-3', 'top-0', 'end-0');
+    const toastContainer = document.createElement("div");
+    toastContainer.classList.add("toast-container", "position-fixed","p-3", "top-0", "end-0");
   
-    const toast = document.createElement('div');
-    toast.classList.add('toast', `bg-${type}`); 
+    const toast = document.createElement("div");
+    toast.classList.add("toast", `bg-${type}`); 
    
-    const toastBody = document.createElement('div');
-    toastBody.classList.add('toast-body', 'text-light');
+    const toastBody = document.createElement("div");
+    toastBody.classList.add("toast-body", "text-light");
     toastBody.textContent = message;
   
     toast.appendChild(toastBody);
     toastContainer.appendChild(toast);
     document.body.appendChild(toastContainer);
 
-    toast.classList.add('show'); 
+    toast.classList.add("show"); 
 
     setTimeout(() => {
-        toast.classList.remove('show'); 
+        toast.classList.remove("show"); 
     }, 3000);
 }
   
