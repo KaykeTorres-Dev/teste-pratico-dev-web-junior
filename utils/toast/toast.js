fb86dd003e04f57e560bca688e3eacce68bf8507
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-01-16 16:25:23 -0300

    Criação do toast

---
+++ utils/toast/toast.js
@@ -0,0 +1,23 @@
+function toast(message, type = 'info') {
+
+    const toastContainer = document.createElement('div');
+    toastContainer.classList.add('toast-container', 'position-fixed','p-3', 'top-0', 'end-0');
+  
+    const toast = document.createElement('div');
+    toast.classList.add('toast', `bg-${type}`); 
+   
+    const toastBody = document.createElement('div');
+    toastBody.classList.add('toast-body', 'text-light');
+    toastBody.textContent = message;
+  
+    toast.appendChild(toastBody);
+    toastContainer.appendChild(toast);
+    document.body.appendChild(toastContainer);
+
+    toast.classList.add('show'); 
+
+    setTimeout(() => {
+        toast.classList.remove('show'); 
+    }, 3000);
+}
+  
